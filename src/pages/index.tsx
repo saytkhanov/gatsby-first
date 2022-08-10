4725bdf0b1db5e4376065a05df0426fa2e040241
work_user <work_user@MacBook-Pro-Shamil.local>
2022-08-10 13:03:47 +0300

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
 import * as React from "react"
 
-export default function Home() {
-    return <div>Hello world!</div>
-}
+const Home = () => {
+    return (
+        <div>
+           Hello world
+        </div>
+    );
+};
+
+export default Home;
